70f5c9011f18e29e5b848f75e9f3be7c2cbb76cf
unknown <jenny.lee@FINOLLC-LEE.finoconsulting.com>
2016-02-29 13:50:34 -0500

    Comment out original GetApi method and add new GetApi method to include api token

--- RallyCat.Core/Rally/RallyApiConnectionPool.cs
+++ RallyCat.Core/Rally/RallyApiConnectionPool.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using RallyCat.Core.Configuration;
@@ -13,43 +14,55 @@ namespace RallyCat.Core.Rally
         public const int MaxConnection = 50;
         public Dictionary<int, RallyApi.RallyRestApi> Pools { get; set; }
 
-        public RallyApi.RallyRestApi GetApi(string userName, string password)
-        {
-            int hash = (userName + "|" + password).GetHashCode();
-            if (Pools == null)
-            {
-                Pools = new Dictionary<int, RallyApi.RallyRestApi>();
-            }
-            if (Pools.ContainsKey(hash) && Pools[hash].ConnectionInfo.UserName == userName && Pools[hash].ConnectionInfo.Password == password)
-                {
-                    var conn = Pools[hash];
-                    if (conn.ConnectionInfo != null &&
-                        conn.AuthenticationState == RallyApi.RallyRestApi.AuthenticationResult.Authenticated)
-                    {
-                        return conn;
-                    }
-                    RallyApi.RallyRestApi.AuthenticationResult r = conn.Authenticate(userName, password, allowSSO:false);
-                    if (r != RallyApi.RallyRestApi.AuthenticationResult.Authenticated)
-                    {
-                        return null;
-                    }
-                }
+        //public RallyApi.RallyRestApi GetApi(string userName, string password)
+        //{
+        //    int hash = (userName + "|" + password).GetHashCode();
+        //    if (Pools == null)
+        //    {
+        //        Pools = new Dictionary<int, RallyApi.RallyRestApi>();
+        //    }
+        //    if (Pools.ContainsKey(hash) && Pools[hash].ConnectionInfo.UserName == userName && Pools[hash].ConnectionInfo.Password == password)
+        //        {
+        //            var conn = Pools[hash];
+        //            if (conn.ConnectionInfo != null &&
+        //                conn.AuthenticationState == RallyApi.RallyRestApi.AuthenticationResult.Authenticated)
+        //            {
+        //                return conn;
+        //            }
+        //            RallyApi.RallyRestApi.AuthenticationResult r = conn.Authenticate(userName, password, allowSSO:false);
+        //            if (r != RallyApi.RallyRestApi.AuthenticationResult.Authenticated)
+        //            {
+        //                return null;
+        //            }
+        //        }
 
-                RallyApi.RallyRestApi api = new RallyApi.RallyRestApi();
-                var result = api.Authenticate(userName, password, allowSSO:false);
-                if (result != RallyApi.RallyRestApi.AuthenticationResult.Authenticated)
-                {
-                    return null;
-                }
-                Pools.Add(hash, api);
+        //        RallyApi.RallyRestApi api = new RallyApi.RallyRestApi();
+        //        var result = api.Authenticate(userName, password, allowSSO:false);
+        //        if (result != RallyApi.RallyRestApi.AuthenticationResult.Authenticated)
+        //        {
+        //            return null;
+        //        }
+        //        Pools.Add(hash, api);
+        //        return api;
+        //    }
+
+        // getapi with token 
+
+        public RallyApi.RallyRestApi GetApi(string apiKey, string rallyServer)
+        {
+            RallyApi.RallyRestApi api = new RallyApi.RallyRestApi();
+            var result = api.AuthenticateWithApiKey("_LrqfvZJdQ8O2UtDHia4uxpNpFBaZUFcoVuEWLpxJy0",
+                "https://rally1.rallydev.com");
+            if (result == RallyApi.RallyRestApi.AuthenticationResult.Authenticated)
+            {
                 return api;
             }
-        
+            else
+            {
+                return null;
+            }
+        }
 
-        
-
-        
     }
-
     
 }
